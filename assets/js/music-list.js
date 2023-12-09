c38340149055e59b4d05dee9931142fd616a9982
ajaydhakal01 <ajaydhakal110@gmail.com>
2023-12-09 17:07:54 +0545

    byAjayDhakal

---
+++ assets/js/music-list.js
@@ -0,0 +1,37 @@
+let allMusic = [
+    {
+        name: "Crown-LegendX",
+        artist: "LegendX",
+        genre: "motivation",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Dreamers(FIFA 2022)",
+        artist: "Jungkook",
+        genre: "vibes",
+        img: "music-2",
+        src: "music-2",
+    },
+    {
+        name: "Sapne",
+        artist: "Amitbhai(Desi Gamers) ft Abhi Payla",
+        genre: "Motivation",
+        img: "music-3",
+        src: "music-3",
+    },
+    {
+        name: "Vardaan",
+        artist: "CarryMinati X Wily Frenzy",
+        genre: "Motivation",
+        img: "music-4",
+        src: "music-4",
+    },
+    {
+        name: "Tu Hai Kahan",
+        artist: "Aur",
+        genre: "Vibes",
+        img: "music-5",
+        src: "music-5",
+    }
+];
